8b013bc26ccc766d86c7a8995de964d649f72795
Anand Sharan <anandsharan97@gmail.com>
2020-12-29 00:11:46 +0100

    added components

--- src/components/Sidebar.component.jsx
+++ src/components/Sidebar.component.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import './Sidebar.component.css';
+import FiberManualRecordIcon from '@material-ui/icons/FiberManualRecord';
+import CreateIcon from '@material-ui/icons/Create';
 
 function Sidebar() {
     return (
         <div className='sidebar'>
             <div className="sidebar__header">
-                
+                <div className="sidebar__info">
+                    <h2>11anands</h2>
+                    <h3>
+                        <FiberManualRecordIcon />
+                        Anand Sharan
+                    </h3>
+                </div> 
+                <CreateIcon />
             </div>
         </div>
     )
