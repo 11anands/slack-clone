ef3eb607c99749e5da72ec614d4c71ec9c83fd85
Anand Sharan <anandsharan97@gmail.com>
2020-12-29 01:00:09 +0100

    Sidebar component Added

--- src/components/Sidebar.component.jsx
+++ src/components/Sidebar.component.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
 import './Sidebar.component.css';
+import SidebarOption  from './SidebarOption.component';
 import FiberManualRecordIcon from '@material-ui/icons/FiberManualRecord';
 import CreateIcon from '@material-ui/icons/Create';
+import InsertCommentIcon from '@material-ui/icons/InsertComment';
+import InboxIcon from '@material-ui/icons/Inbox';
+import DraftIcon from '@material-ui/icons/Drafts';
+import BookmarkBorderIcon from '@material-ui/icons/BookmarkBorder';
+import PeopleAltIcon from '@material-ui/icons/PeopleAlt';
+import AppsIcon from '@material-ui/icons/Apps';
+import FileCopyIcon from '@material-ui/icons/FileCopy';
+import ExpandLessIcon from '@material-ui/icons/ExpandLess';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
 function Sidebar() {
     return (
@@ -16,6 +26,16 @@ function Sidebar() {
                 </div> 
                 <CreateIcon />
             </div>
+            <SidebarOption Icon={InsertCommentIcon} title={"Threads"}/>
+            <SidebarOption Icon={InboxIcon} title={"Mention & Reactions"}/>
+            <SidebarOption Icon={DraftIcon} title={"Saved Items"}/>
+            <SidebarOption Icon={BookmarkBorderIcon} title={"Channel Browser"}/>
+            <SidebarOption Icon={PeopleAltIcon} title={"People & User Group"}/>
+            <SidebarOption Icon={AppsIcon} title={"Apps"}/>
+            <SidebarOption Icon={FileCopyIcon} title={"File Browser"}/>
+            <SidebarOption Icon={ExpandLessIcon} title={"Show Less"}/>
+            <hr/>
+            <SidebarOption Icon={ExpandMoreIcon} title={"Channels"}/>
         </div>
     )
 }
